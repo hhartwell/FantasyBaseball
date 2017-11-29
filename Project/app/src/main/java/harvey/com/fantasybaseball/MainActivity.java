5365a440b00e91bc43af71a10cbbceccddb30295
hhartwell <hhartwell@zagmail.gonzaga.edu>
2017-11-28 18:32:30 -0800

    added functions into main activity for menu clicks

--- Project/app/src/main/java/harvey/com/fantasybaseball/MainActivity.java
+++ Project/app/src/main/java/harvey/com/fantasybaseball/MainActivity.java
@@ -5,16 +5,28 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     // still need to create the listView
+
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = (ListView) findViewById(R.id.teams_list_view);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                createDummyList()
+        );
 
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 
@@ -28,9 +40,35 @@ public class MainActivity extends AppCompatActivity {
         int menuId = item.getItemId();
         switch(menuId){
             case R.id.createTeamAction:
+                // needs to launch the Team activity
+            case R.id.sendScoreAction:
+                // sends the winning team's score to all players
+                sendScores();
+                Toast.makeText(this, "Winning Scores sent", Toast.LENGTH_SHORT).show();
+            default:
 
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * function used for testing purposes only.
+     * creates a super simple list as a place holder
+     * @return
+     */
+    private ArrayList<String> createDummyList(){
+        ArrayList<String> dummyList = new ArrayList<String>();
+        for (int i = 0; i < 20; i++) {
+            dummyList.add("team: " + i);
+        }
+
+        return dummyList;
+    }
+    /**
+     * sends out the winning team via text
+     */
+    private void sendScores(){
+
     }
 }
