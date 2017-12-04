43db136b32741010dd3eaf5bc7630e98698730df
Daniel Richard <drichard989@gmail.com>
2017-12-04 15:49:01 -0800

    updated draftteam
    dialogs

--- Project/app/src/main/java/harvey/com/fantasybaseball/DraftTeam.java
+++ Project/app/src/main/java/harvey/com/fantasybaseball/DraftTeam.java
@@ -1,47 +1,106 @@
 package harvey.com.fantasybaseball;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class DraftTeam extends AppCompatActivity {
 
 
     int playersDrafted =0;
+    boolean teamCreated;
 
-// activity used to draft a team
+    // activity used to draft a team
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_draft_team);
+        teamCreated =false;
+        if (playersDrafted <=10){
+            TextView textView = (TextView) findViewById(R.id.prompt);
+            String message = "Please select a batter, you have "+ (10-playersDrafted) + " batters remaining";
+        }
+        if (playersDrafted >=11 && playersDrafted <15){
+            TextView textView = (TextView) findViewById(R.id.prompt);
+            String message = "Please select a pitcher, you have "+ (5-playersDrafted) + " batters remaining";
+        }
     }
 
-    public void insertIntoTeamsTable(){
-        //insert phone number, team name... into table
 
+
+
+    public void draftPlayerFromTable(){ // called when user presses draft team
+        //update row in table with team ID of player to indicate its drafted
+        //update listView
         EditText teamName = (EditText) findViewById(R.id.teamName);
         EditText phoneNumber = (EditText) findViewById(R.id.phone);
+        EditText PID = (EditText) findViewById(R.id.playerID);
+
+
+        if (phoneNumber.getText().toString().toString()==null){
+            //DIALOG "ENTER A PHONE NUMBER"
+
+            AlertDialog.Builder dialog=new AlertDialog.Builder(DraftTeam.this);
+            dialog.setTitle("Enter Number");
+            dialog.setMessage("Please enter a phone number to draft a player");
+            dialog.setPositiveButton("OK", null);
+            dialog.show();
+
+            return;
+        }
+        if (teamName.getText().toString()==null){
+            //Dialog "Enter a team name"
+            AlertDialog.Builder dialog=new AlertDialog.Builder(DraftTeam.this);
+            dialog.setTitle("Enter Team Name");
+            dialog.setMessage("Please enter a Team Name to draft a player");
+            dialog.setPositiveButton("OK", null);
+            dialog.show();
+            return;
+        }
+        if(PID.getText().toString()==""){
+            //Dialog "You must enter the playerID that you want to draft
+            AlertDialog.Builder dialog=new AlertDialog.Builder(DraftTeam.this);
+            dialog.setTitle("Enter Team Name");
+            dialog.setMessage("Please enter a Team Name to draft a player");
+            dialog.setPositiveButton("OK", null);
+            dialog.show();
+            return;
+
+        }
+
 
         int phone= Integer.parseInt(phoneNumber.getText().toString());
         String team = teamName.getText().toString();
+        int playerID=Integer.parseInt(PID.getText().toString());
 
-        //SQL INSERT LOGIC GOES BELOW
+        if (teamCreated==false){
 
-    }
+            teamCreated=true;
+
+            //SQL INSERT LOGIC GOES BELOW
+        }
+        if (playersDrafted <=10){
+            TextView textView = (TextView) findViewById(R.id.prompt);
+            String message = "Please select a batter, you have "+ (10-playersDrafted) + " batters remaining";
+        }
+        if (playersDrafted >=11 && playersDrafted <15){
+            TextView textView = (TextView) findViewById(R.id.prompt);
+            String message = "Please select a pitcher, you have "+ (5-playersDrafted) + " batters remaining";
+        }
 
 
-    public void draftPlayerFromTable(){
-        //update row in table with team ID of player to indicate its drafted
-        //update listView
-        EditText phoneNumber = (EditText) findViewById(R.id.phone);
 
-
-        EditText playerID = (EditText) findViewById(R.id.playerID);
-        int PID = Integer.parseInt(playerID.getText().toString());
-        int phone= Integer.parseInt(phoneNumber.getText().toString());
         playersDrafted++;
-
         //update row in DB with the PID and add the phone to the UID cell to show it is drafted.
+
+
+        if (playersDrafted==15){
+            //return to main menu
+
+
+        }
 
 
     }
